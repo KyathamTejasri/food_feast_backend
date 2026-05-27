c1ee4f859a32e3e2bc6fdb20dfcb4e69e8c48634
Tejasri Kyatham <tejasrikyatham123@gmail.com>
2026-05-27 22:54:47 +0530

    updated cors issue

--- src/server.js
+++ src/server.js
@@ -22,10 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Enable CORS
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors({origin: "*"}));
 
 // Routes
 app.use('/api/auth', authRoutes);
